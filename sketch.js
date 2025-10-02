a0b5fa50e1236020334dfd4114f5242188cd3f4c
boccos <145885073+boccos@users.noreply.github.com>
2025-10-01 22:45:04 -0400

    Added more user interactivity
    User can now change drawing colours and clear the sketch pad.

--- sketch.js
+++ sketch.js
@@ -1,19 +1,34 @@
 const start = document.getElementById("start");
-const container = document.querySelector("#container");
+
+const mainContainer = document.createElement("div");
+mainContainer.setAttribute("id", "mainContainer");
+document.body.appendChild(mainContainer);
+
+const container = document.createElement("div");
+mainContainer.appendChild(container);
+
+const bottomBar = document.createElement("div");
+bottomBar.setAttribute("class", "bottomBar");
+mainContainer.appendChild(bottomBar);
+
+const buttonContainer = document.createElement("div");
 
 
 let mouseDown = false;
+let redActive = false;
+let blueActive = false;
+let greenActive = false;
+let randomActive = true;
+
 document.body.addEventListener("mousedown", () => mouseDown = true);
 document.body.addEventListener("mouseup", () => mouseDown = false);
 
 start.addEventListener("click", () => {
     start.remove();
-    console.log("test");
     gridSize();
 })
 
 function gridSize(){
-    const buttonContainer = document.createElement("buttonContainer");
     buttonContainer.setAttribute("id", "buttonContainer");
     container.appendChild(buttonContainer);
 
@@ -47,9 +62,75 @@ function gridSize(){
 
 function initStart(){
     buttonContainer.remove();
+
+    const red = document.createElement("button");
+    red.setAttribute("class", "bottomBar red");
+    red.textContent = "Red";
+    bottomBar.appendChild(red);
+
+    const blue = document.createElement("button");
+    blue.setAttribute("class", "bottomBar blue");
+    blue.textContent = "Blue";
+    bottomBar.appendChild(blue);
+
+    const green = document.createElement("button");
+    green.setAttribute("class", "bottomBar green");
+    green.textContent = "Green";
+    bottomBar.appendChild(green);
+
+    const randomColor = document.createElement("button");
+    randomColor.setAttribute("class", "bottomBar randomColor");
+    randomColor.textContent = "?";
+    bottomBar.appendChild(randomColor);
+
+    const clear = document.createElement("button");
+    clear.setAttribute("class", "bottomBar clear");
+    clear.textContent = "Clear";
+    bottomBar.appendChild(clear);
+
+    red.addEventListener("click", () => {
+        redActive = true;
+        blueActive = false;
+        greenActive = false;
+        randomActive = false;
+    });
+
+    blue.addEventListener("click", () => {
+        redActive = false;
+        blueActive = true;
+        greenActive = false;
+        randomActive = false;
+    });
+
+    green.addEventListener("click", () => {
+        redActive = false;
+        blueActive = false;
+        greenActive = true;
+        randomActive = false;
+    });
+
+    randomColor.addEventListener("click", () => {
+        redActive = false;
+        blueActive = false;
+        greenActive = false;
+        randomActive = true;
+    });
+
+    clear.addEventListener("click", () => {
+        redActive = false;
+        blueActive = false;
+        greenActive = false;
+        randomActive = false;
+
+        const allCols = container.querySelectorAll(".col");
+        allCols.forEach(col => {
+            col.style.backgroundColor = "white";
+        });
+    });
 }
 
 function newGrid(size){
+    container.innerHTML = "";
     for (let i = 0; i < size; i++){
         const row = document.createElement("div");
         row.setAttribute("class", "row");
@@ -61,7 +142,18 @@ function newGrid(size){
 
             col.addEventListener("mouseover", () => {
                 if (mouseDown){
-                    col.style.backgroundColor = "aquamarine";
+                    if (redActive) {
+                        col.style.backgroundColor = "rgb(255, 117, 117)";
+                    } else if (blueActive) {
+                        col.style.backgroundColor = "rgb(131, 131, 255)";
+                    } else if (greenActive) {
+                        col.style.backgroundColor = "rgb(131, 255, 131)";
+                    } else if (randomActive) {
+                        const r = Math.floor(Math.random() * 256);
+                        const g = Math.floor(Math.random() * 256);
+                        const b = Math.floor(Math.random() * 256);
+                        col.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+                    }
                 }
             })
         }
